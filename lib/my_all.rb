8d298a1d7e66e99d55282f8049537961877ebc71
Matt Maher <mattmaher0@gmail.com>
2018-06-04 21:59:22 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  results = []
+  while i < collection.size
+    results << yield(collection[i])
+    i = i + 1 
+  end  
+  results.all?
+  
 end
